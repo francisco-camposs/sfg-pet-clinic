117f019d2fac972b32c218d915f9361a129fb6c0
Francisco Campos <campos.junior.ifrn@gmail.com>
2022-08-24 21:34:27 -0300

    Adding vets page and controller. Closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping(path = {"/vets", "/vets/index", "/vets/index.html"}, method = RequestMethod.GET)
+    public String listVets() {
+        return "vets/index";
+    }
+
+}
