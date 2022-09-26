ec846aedd8a719eb30554bf8980657a296c7c96c
francisco <campos.junior.ifrn@gmail.com>
2022-09-26 08:59:37 -0300

    Added spring jpa service for PetType. Closes #42

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -3,6 +3,6 @@ package guru.springframework.sfgpetclinic.services;
 import guru.springframework.sfgpetclinic.model.BaseEntity;
 import guru.springframework.sfgpetclinic.model.PetType;
 
-public interface PetTypeService<P extends BaseEntity, L extends Number> extends CrudService<PetType, Long> {
+public interface PetTypeService extends CrudService<PetType, Long> {
 
 }
